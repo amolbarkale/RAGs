aa1d53b6cea41b4e6d471c4eb7f4c25e10387334
amolbarkale <barkaleamol@gmail.com>
2025-07-08 15:35:41 +0530

    setup project for semantic similarity analyzer

---
+++ Plagiarism Detector - Semantic Similarity Analyzer/README.md
@@ -0,0 +1,3 @@
+curl -Ls https://astral.sh/uv/install.sh | sh
+
+uv run main.py - automatically creates virtual env
---
+++ Plagiarism Detector - Semantic Similarity Analyzer/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from plagiarism-detector-semantic-similarity-analyzer!")
+
+
+if __name__ == "__main__":
+    main()
