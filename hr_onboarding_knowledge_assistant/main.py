21234d8dca18db84cbbcd18074d2b1bc1fbedfdb
amolbarkale <barkaleamol@gmail.com>
2025-07-09 16:13:57 +0530

    rename folder

---
+++ hr_onboarding_knowledge_assistant/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from hr-onboarding-knowledge-assistant!")
+
+
+if __name__ == "__main__":
+    main()
--- rag_chunking_strategies/app.py
+++ rag_chunking_strategies/app.py
@@ -1,4 +1,3 @@
-# app.py
 import streamlit as st
 from pathlib import Path
 from engine.load_and_split import load_pdf, get_chunks
